545a5c2c59bddcbcf4bb734a47ca213313091c0d
GrO0vy <lmc2819@naver.com>
2023-07-05 20:33:32 +0900

    fix: NotNull 인 값만 표시 되도록 수정

--- mutsaMarket/src/main/java/com/example/mutsaMarket/dto/NegotiationDto.java
+++ mutsaMarket/src/main/java/com/example/mutsaMarket/dto/NegotiationDto.java
@@ -1,8 +1,10 @@
 package com.example.mutsaMarket.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class NegotiationDto {
     private Integer id;
     private Integer itemId;
