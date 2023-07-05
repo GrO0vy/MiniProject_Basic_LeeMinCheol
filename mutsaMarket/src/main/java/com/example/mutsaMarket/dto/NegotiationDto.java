3841a38f0d7cceb0cb86ec9b8679213fa221cd72
GrO0vy <lmc2819@naver.com>
2023-07-05 20:49:14 +0900

    fix: NegotiationDto 에 정적 팩토리 메서드 추가

--- mutsaMarket/src/main/java/com/example/mutsaMarket/dto/NegotiationDto.java
+++ mutsaMarket/src/main/java/com/example/mutsaMarket/dto/NegotiationDto.java
@@ -1,5 +1,6 @@
 package com.example.mutsaMarket.dto;
 
+import com.example.mutsaMarket.entity.NegotiationEntity;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
@@ -12,4 +13,17 @@ public class NegotiationDto {
     private String status;
     private String writer;
     private String password;
+
+    public static NegotiationDto fromEntity(NegotiationEntity entity){
+        NegotiationDto negotiationDto = new NegotiationDto();
+
+        negotiationDto.setId(entity.getId());
+        negotiationDto.setItemId(entity.getItemId());
+        negotiationDto.setSuggestedPrice(entity.getSuggestedPrice());
+        negotiationDto.setStatus(entity.getStatus());
+        negotiationDto.setWriter(entity.getWriter());
+        negotiationDto.setPassword(entity.getPassword());
+
+        return negotiationDto;
+    }
 }
