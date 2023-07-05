9538b00fc5a7b7bfed7294d441ee24d34dfc768c
GrO0vy <lmc2819@naver.com>
2023-07-05 20:57:13 +0900

    fix: 물품이 존재하지 않을 때 구매제안 할 수 없도록 수정

--- mutsaMarket/src/main/java/com/example/mutsaMarket/services/NegotiationService.java
+++ mutsaMarket/src/main/java/com/example/mutsaMarket/services/NegotiationService.java
@@ -3,15 +3,22 @@ package com.example.mutsaMarket.services;
 import com.example.mutsaMarket.dto.NegotiationDto;
 import com.example.mutsaMarket.entity.NegotiationEntity;
 import com.example.mutsaMarket.repositories.NegotiationRepository;
+import com.example.mutsaMarket.repositories.SalesItemRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
 public class NegotiationService {
     private final NegotiationRepository negotiationRepository;
+    private final SalesItemRepository salesItemRepository;
 
     public NegotiationDto registerNegotiation(Integer itemId, NegotiationDto negotiationDto){
+        if(!salesItemRepository.existsById(itemId))
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
         NegotiationEntity entity = new NegotiationEntity();
         entity.setItemId(itemId);
         entity.setSuggestedPrice(negotiationDto.getSuggestedPrice());
