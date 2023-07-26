286e3dc09691f9c1dca61ed4962431d0ee4111ad
GrO0vy <lmc2819@naver.com>
2023-07-26 11:37:37 +0900

    fix: 물품 삭제 시 이미지파일과 디렉토리도 함께 삭제 되도록 수정

--- mutsaMarket/src/main/java/com/example/mutsaMarket/services/SalesItemService.java
+++ mutsaMarket/src/main/java/com/example/mutsaMarket/services/SalesItemService.java
@@ -145,7 +145,24 @@ public class SalesItemService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
-        salesItemRepository.delete(optionalEntity.get());
+        SalesItemEntity entity = optionalEntity.get();
+
+        // 이미지 삭제
+        try{
+            // 파일 삭제
+            String file = entity.getImageUrl();
+            file = file.replace("static", "itemImages");
+            // 디렉토리 삭제
+            Files.deleteIfExists(Path.of(file));
+            String directory = file.substring(0, file.indexOf("/image.png"));
+            Files.deleteIfExists(Path.of(directory));
+        }
+        catch(Exception e){
+            log.warn(e.getMessage());
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        salesItemRepository.delete(entity);
         commentRepository.deleteAllByItemId(itemId);
     }
 
